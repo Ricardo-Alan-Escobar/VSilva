b1fe13d7c37f8c1692f8f91067bca7e1d576534b
Ricardo Alan Escobar <ricardoalangomezescobar@gmail.com>
2025-02-24 17:04:26 -0600

    Cambios en header

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'; // Importa el complemento de Tailwind CSS
 import icon from "astro-icon";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+
   integrations: [icon()],
   vite: {
     
--- public/scroll-header.js
+++ public/scroll-header.js
@@ -1,13 +1,15 @@
-document.addEventListener('scroll', function () {
-  const header = document.getElementById('header');
-  if (window.scrollY > 50) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('scroll', function () {
+    const header = document.getElementById('header');
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
 
-document.getElementById('menu-toggle').addEventListener('click', function () {
-  const header = document.getElementById('header');
-  header.classList.toggle('open');
+  document.getElementById('menu-toggle').addEventListener('click', function () {
+    const header = document.getElementById('header');
+    header.classList.toggle('open');
+  });
 });
